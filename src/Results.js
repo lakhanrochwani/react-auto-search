49594411fa829b79539d7cf73b93d8dc3d3247db
lakhanrochwani <rochwanilakhan@gmail.com>
2022-01-11 22:46:12 -0600

    1 file modified

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 const Results = ({ text, input }) => {
-  let styled = text.replaceAll(input, '<strong>' + input + '</strong>');
+  let styled = text.replaceAll(input, bold(input));
+  function bold(str) {
+    // debugger;
+
+    let fontColor = str.fontcolor('red');
+    let element = document.getElementsByTagName('font');
+    element.innerHTML = `<b>${input}</b>`;
+    console.log(element);
+    console.log(fontColor);
+    return element;
+  }
   return (
     <>
       <li>{styled}</li>
-      {console.log('Styled:', styled)}
+      {/* {console.log('Styled:', styled)} */}
     </>
   );
 };
